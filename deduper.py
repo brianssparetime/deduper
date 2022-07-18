00f61d2c652f94ec1189f6ed00b2fe018fcc7480
Brian <brianssparetime@gmail.com>
2022-07-17 23:43:33 -0700

    Added record-deleted, moved from os.replace to shutil.move, cleaned up broken shit, misc improvements

--- deduper.py
+++ deduper.py
@@ -7,6 +7,8 @@ import operator
 import argparse
 import hashlib
 import operator
+import datetime
+import shutil
 
 DEBUG = False
 
@@ -28,7 +30,8 @@ img_exts = [
     '.pdf',
 ]
 
-dupe_indicator = '!'
+
+dupe_indicator = '!' 
 max_marks = 14
 
 
@@ -37,6 +40,7 @@ ap.add_argument('-t','--type', required=False, help='img or vid, img is default'
 ap.add_argument('-r','--recursive', action='store_true', default=False, help='recursive')
 ap.add_argument('--rename-delete', action='store_true', default=False, help='action type')
 ap.add_argument('-s','--simulate', action='store_true', default=False, help='simulate')
+ap.add_argument('-w','--record-deleted', action='store_true', default=False, help='write out deleted files to .txt')
 ap.add_argument('-z','--track-size', action='store_true', default=False, help='track size of deletions')
 ap.add_argument('target', help='directory to search')
 args = ap.parse_args()
@@ -44,8 +48,10 @@ total_size_deleted = 0
 
 if args.type == 'img':
     exts = img_exts
+    dupe_indicator = '!' 
 elif args.type == 'vid':
     exts = vid_exts
+    dupe_indicator = '&' 
 else:
     print("--type must be either 'img' or 'vid'!")
     sys.exit()
@@ -95,61 +101,6 @@ def count_marks(fn):
     # TODO:  probably need special regex handling below for special characters....
     m = max( [len(x) for x in re.findall(r'['+dupe_indicator+']+', fn)] , default=0)
     return m
-    # let's handle mark reduction elsewhere
-    #return 0 if m < 2 else m
-
-# def choose_filename(h):
-#     # sort by length, remove basedir
-#     fns = sorted( sorted([ os.path.split(fn)[1] for fn in  matches[h] ]) , key=len)
-#     basedir,_ = os.path.split(matches[h][0])
-#     basename , ext = os.path.splitext(fns[0])
-#     bestname = basename.strip()
-#     # print("starting basename = {}".format(basename))
-#     # print("starting bestname = {}".format(bestname))
-#     # print(fns)
-
-#     marks = count_marks(bestname)
-
-#     # for longer names
-#     for n in fns[1:]:
-#         fpart, ext = os.path.splitext(n)
-#         # print("fpart = {}".format(fpart))
-#         delta = fpart.replace(basename, '').strip()
-#         # count marks
-#         marks = max(marks, count_marks(delta))
-#         # remove junk
-#         delta = re.sub(r'[\W\S]+',' ', delta).strip()
-#         if re.match(r'\s*$', delta):
-#             continue
-
-
-#         # print("clean delta = {}".format(delta))
-#         #if re.search(r'copy\d?', delta, re.I):
-#         if re.match(r'^copy\s*\d?$', delta, re.I):
-#             continue
-#         if re.match(r'^\d+$', delta):
-#             continue
-#         else:
-#             bestname = ' '.join([bestname, delta])
-
-#     return (os.path.join(basedir, bestname+ext), marks)
-
-# def clean_fpart_name(n):
-#     # remove marks from delta
-#     n = n.replace(dupe_indicator, '')
-#     # remove non alpha non space junk
-#     n = re.sub(r'[^\w\s]+',' ', n)
-#     if re.match(r'^\s*$', n):
-#         return ''
-#     # eliminate dupe conventions
-#     n = re.sub(r'copy','', n, re.I) 
-#     # this is broke,but try to kill single/double digits at the end
-#     n = re.sub(r'\s+-?\d+\s+', ' ', n)
-#     # consolidate spaces
-#     n = re.sub(r'\s+',' ', n)
-#     n = n.strip()
-#     return n
-
 
 
 def most_common_long_prefix(fns):
@@ -186,80 +137,23 @@ def most_common_long_prefix(fns):
     return mclp[0]
 
 
-"""
 
-## TODO:
-
-# this is a pretty broken attempt....
-def longest_common_prefix(fns):
-    if not fns:
-        return ''
-    elif len(fns) == 1:
-        return fns[0]
-
-    arr = fns.sort()
-    result = ""
-    # Compare the first and the last string character
-    # by character.
-    for i in range(len(arr[0])):
-        #  If the characters match, append the character to
-        #  the result.
-        if arr[0][i] == arr[-1][i]:
-            result += arr[0][i]
-        # Else, stop the comparison
-        else:
-            break
-    return result.strip()
-
-
-# need to find not longest common prefix, but rather most common long prefix...
-
-# consider
-
-# foobar
-# long_prefix_with_stuff here
-# long_prefix_with_stuff here 1
-# long_prefix_with_stuff here 2
-# long_prefix_with_stuff here 3
-# zeds dead baby
-
-# maybe...
-# find longest common prefix over whole list
-# if zero, remove top or bottom...
-
-# or 
-
-# dictionary
-# for each string
-    # for each token
-        # add string up to and including token to dictionary or increment count if exists
-# take key with highest value count, break ties with longest
-
-"""
-
-
-
-
-def choose_starting_name(fns):
+def choose_dir_and_ext(fns):
     """ currently using only basedir from here...."""
     if len(fns) == 1:
         return fns[0]
     
     rootiest_dir = ''
-    longest_fpart = ''
     final_ext = ''
     for f in fns:
         dir, fn = os.path.split(f)
         fpart, ext = os.path.splitext(fn)
-        if len(dir) < len(rootiest_dir):
+        if rootiest_dir == '' or len(dir) < len(rootiest_dir):
             # replace only if none selected or candidate is alphabetically less than
             if not rootiest_dir or dir < rootiest_dir:
                 rootiest_dir = dir
-
-        if len(fpart) > len(longest_fpart):
-            longest_fpart = fpart
-            final_ext = ext
-    return (rootiest_dir, longest_fpart, final_ext)
+        final_ext = ext
+    return (rootiest_dir, final_ext)
 
 
 
@@ -271,7 +165,7 @@ def choose_starting_name(fns):
 def choose_filename_new(h):
 
     fns = [f for f in matches[h]]
-    (final_basedir, _, final_ext) = choose_starting_name(fns)
+    (final_basedir, final_ext) = choose_dir_and_ext(fns)
 
 
     # separate out paths, fparts and exts
@@ -354,7 +248,21 @@ def add_marks_to_name(n, h, carryover_marks):
         return bn + ext
 
 
+def record_deleted(action_type, oldname, newname, hash):
+    record_file_name = 'deleted_duplicate_files.txt'
+    old_path, _ = os.path.split(oldname)
+    yyyymmdd = datetime.datetime.today().strftime('%Y-%m-%d')
 
+    # TODO: handle unicode better
+    # https://stackoverflow.com/questions/6048085/writing-unicode-text-to-a-text-file
+
+    with open(record_file_name, 'a') as f:
+        f.write(f'{action_type}\t{oldname}\t{newname}\t{hash}\t{yyyymmdd}\n')
+
+
+
+# TODO:  consider how to handle overlaps of delete, rename, and record.
+# TODO:  should simulate record separately than a real delete?  Or at all?
 
 
 def match_action(h):
@@ -363,14 +271,20 @@ def match_action(h):
     if args.rename_delete:
         (newname, carryover_marks) = choose_filename_new(h)
         newname = add_marks_to_name(newname, h, carryover_marks)
+        oldname = matches[h][0]
         try:
-            print("\t Move: {} to {} ".format(matches[h][0], newname))
+            print("\t Move: {} to {} ".format(oldname, newname))
+            if args.record_deleted:
+                record_deleted('RENAMED', oldname, newname, h)
             if not args.simulate:
-                os.replace(matches[h][0], newname)
+                #os.replace(oldname, newname)
+                shutil.move(oldname, newname)
             for x in matches[h][1:]:
                 print("\t rm:   {} ".format(x))
                 if args.track_size:
                     total_size_deleted += os.path.getsize(x)
+                if args.record_deleted:
+                    record_deleted('DELETED', x, newname, h)
                 if not args.simulate:
                     os.remove(x)
         except Exception as e:
@@ -418,16 +332,16 @@ for fn, fsize in sorted_tuples:
 
 
 
-def convert_size(size_bytes):
-    import math
-    if size_bytes == 0:
-        return "0B"
-    size_name = ("B", "KB", "MB", "GB", "TB")
-    i = int(math.floor(math.log(size_bytes, 1024)))
-    p = math.pow(1024, i)
-    s = round(size_bytes / p, 2)
-    return "%s %s" % (s, size_name[i])
-
 if args.track_size:
-    #print("total space saved = {}".format(hurry.filesize.size(total_size_deleted)))
+
+    def convert_size(size_bytes):
+        import math
+        if size_bytes == 0:
+            return "0B"
+        size_name = ("B", "KB", "MB", "GB", "TB")
+        i = int(math.floor(math.log(size_bytes, 1024)))
+        p = math.pow(1024, i)
+        s = round(size_bytes / p, 2)
+        return "%s %s" % (s, size_name[i])
+
     print("total space saved = {}".format(convert_size(total_size_deleted)))
